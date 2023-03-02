807cc9079837d79831f2ee16c07c20b65aaa5f0a
BMVBrun0 <102544782+BMVBrun0@users.noreply.github.com>
2023-03-02 11:26:19 -0300

    safeareaview

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
-import { Routes } from './src/Routes'
+import { Routes } from './src/Routes';
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
     return(
-        <Routes/>
+        <SafeAreaProvider>
+            <SafeAreaView style={{flex:1, backgroundColor:'black'}}>
+                <Routes/>
+            </SafeAreaView>
+        </SafeAreaProvider>
+        
     );
 }
